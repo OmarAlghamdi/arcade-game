d79f2cad0afe59d37a2559016d4d6f240a0c7a66
OmarAlghamdi <mr.alghamdi.omar@gmail.com>
2019-10-27 04:56:39 +0300

    v1.1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function(i) {
+var Enemy = function (i) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -13,7 +13,7 @@ var Enemy = function(i) {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function () {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -23,7 +23,7 @@ Enemy.prototype.update = function(dt) {
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -39,41 +39,66 @@ class Player {
         this.ySpeed = 78;
         this.xOnUpdate = 0;
         this.yOnUpdate = 0;
+        this.winner = false;
     }
-    update(dt) {
+    update() {
         this.x += this.xOnUpdate;
         this.y += this.yOnUpdate;
         this.xOnUpdate = 0;
         this.yOnUpdate = 0;
+        // detect win state
+        if (this.y == 0)
+            this.win();
     }
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        if (this.winner) {
+            ctx.fillStyle = 'yellow';
+            ctx.font = '48px serif';
+            ctx.fillText('You Won!!', 150, 520);
+        }
     }
     handleInput(key) {
+        // stop moving on lose
+        if (stop)
+            return;
         switch (key) {
             case 'left':
                 if (this.x > 0)
                     this.xOnUpdate = -(this.xSpeed);
                 break;
-            
+
             case 'right':
                 if (this.x < 404)
                     this.xOnUpdate = this.xSpeed;
                 break;
-            
+
             case 'down':
                 if (this.y < 390)
                     this.yOnUpdate = this.ySpeed;
                 break;
-            
+
             case 'up':
                 if (this.y > 0)
                     this.yOnUpdate = -(this.ySpeed);
                 break;
-        
+
             default:
                 break;
         }
+    }
+    // show win message
+    win() {
+        this.winner = true;
+        setTimeout(() => {
+            this.winner = false;
+            this.goBack();
+        }, 3000)
+    }
+    // take player back to the start point
+    goBack() {
+        this.x = 101 * 2;
+        this.y = 78 * 4;
     }
 }
 
@@ -90,7 +115,7 @@ for (let i = 0; i < 3; i++) {
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
